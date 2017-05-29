aef820b228e6afdfae0b77a61bd157f18653fb16
Dominik C <d.cwiklik@promoagency.pl>
2017-05-29 14:25:42 +0200

    init structure

---
+++ Module.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace dcwq\extracms;
+
+/**
+ * Class Module
+ *
+ * @package dcwq\extracms
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @var string the namespace that controller classes are in
+     */
+    public $controllerNamespace = 'dcwq\extracms\controllers';
+
+}
